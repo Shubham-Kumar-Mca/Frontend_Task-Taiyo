241c11e20e9a58caa08cd151f868aa3ad4c89e84
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-29 15:15:33 +0530

    Action Type Here..

---
+++ contact-manager/src/Redux/actionType.ts
@@ -0,0 +1,4 @@
+export const CREATE_CONTACT = "create_contact";
+export const GET_CONTACT = "getting_contact";
+export const UPDATE_USER = "update_contact";
+export const DELETE_USER = "delete_contact";
